c75022e0858a5ec063a236041b526950ba06807a
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-07-25 15:30:13 +0530

    Time: 110 ms (33.97%), Space: 25.3 MB (16.57%) - LeetHub

---
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    bool isValid(string s, string str){
+        int i = 0, j = 0;
+        while(i < s.length() && j < str.length()){
+            if(s[i] == str[j]){
+                i++;
+                j++;
+            }
+            else i++;
+        }
+        return j == str.length() ? true : false;
+    }
+    
+    string findLongestWord(string s, vector<string>& dictionary) {
+        string res = "";
+        sort(dictionary.begin(), dictionary.end());
+        for(auto str: dictionary){
+            if(isValid(s, str)){
+                if(res.length() < str.length()) res = str;
+            }
+        }
+        return res;
+    }
+};
