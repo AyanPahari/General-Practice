c569a3eee87202b6b44a8fd45175eca81f7e6cef
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-08-23 19:59:23 +0530

    Time: 59 ms (51.05%), Space: 21.5 MB (65.58%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int i = 0;
+        while(i < nums.size()){
+            if(i == nums[i] - 1 || nums[i] == nums[nums[i] - 1]){
+                i++;
+                continue;
+            }
+            swap(nums[i], nums[nums[i] - 1]);
+        }
+        vector<int>res;
+        for(int i=0;i<nums.size();i++){
+            if(i != nums[i] - 1){
+                res.push_back(nums[i]);
+                res.push_back(i + 1);
+            }
+        }
+        return res;
+    }
+};
