8ed081e66e38599058712233af87c31eddf7e784
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-08-24 14:42:17 +0530

    Time: 15 ms (100.00%), Space: 21.7 MB (99.93%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -4,12 +4,11 @@ public:
         int n = nums.size();
         int i = 0;
         while(i < n){
-            if(nums[i] <= 0 || nums[i] > n || nums[i] == i + 1){
+            if(nums[i] <= 0 || nums[i] > n || nums[i] == i + 1 || nums[i] == nums[nums[i] - 1]){
                 i++;
                 continue;
             }
-            if(nums[i] != nums[nums[i] - 1]) swap(nums[i], nums[nums[i] - 1]);
-            else i++;
+            swap(nums[i], nums[nums[i] - 1]);
         }
         for(int i=0;i<n;i++){
             if(nums[i] != i + 1) return i + 1;
