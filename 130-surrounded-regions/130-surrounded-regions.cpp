a093555c7c86d2d5e4edfcaa40f5119ec5628798
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-08-30 12:48:57 +0530

    Time: 24 ms (38.95%), Space: 10.1 MB (39.72%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int vis[201][201];
+    int row, col;
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    
+    bool isValid(int x, int y, vector<vector<char>>& grid){
+        if(x < 0 || y < 0 || x >= row || y >= col) return false;
+        if(vis[x][y] == 1 || grid[x][y] == 'X' || grid[x][y] == '#') return false;
+        return true;
+    }
+    
+    void dfs(int x, int y, vector<vector<char>>& grid){
+        vis[x][y] = 1;
+        grid[x][y] = '#';
+        for(int i=0;i<4;i++){
+            if(isValid(x + dx[i], y + dy[i], grid)) dfs(x + dx[i], y + dy[i], grid);
+        }
+    }
+    
+    void solve(vector<vector<char>>& board) {
+        row = board.size(), col = board[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j] == 'O'){
+                    if(i == 0 || j == 0 || i == row - 1 || j == col - 1) dfs(i, j, board);
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                else if(board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+    }
+};
